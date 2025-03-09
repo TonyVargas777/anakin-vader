cd5f4ca2cd42bbed49a97e8fc1909438b34c03c3
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2025-03-09 22:49:13 +0100

    css2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import 'two-up-element'
-import './App.css'
-import { useRef } from 'react'
+import "two-up-element";
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  const audioRef = useRef(null)
+  const audioRef = useRef(null);
 
   const playAudio = () => {
     if (audioRef.current) {
-      audioRef.current.play()
+      audioRef.current.play();
     }
-  }
+  };
 
   return (
     <>
-      <two-up>
-        <img src="cromo-anakin.jpg" alt="anakin" />
-        <img src="obiwan.webp" alt="vader" />
-      </two-up>
-      <button onClick={playAudio}>Reproducir música</button>
-      <audio ref={audioRef} src="Star Wars - Battle of the Heroes.mp3"></audio>
+      <div className="box">
+        <two-up>
+          <img src="cromo-anakin.jpg" alt="anakin" />
+          <img src="obiwan.webp" alt="vader" />
+        </two-up>
+        <button onClick={playAudio}>Reproducir música</button>
+        <audio
+          ref={audioRef}
+          src="Star Wars - Battle of the Heroes.mp3"
+        ></audio>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
